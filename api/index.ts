b77621c11320f29ef9f6c676b6e1e4fb27897555
Camila <camilalobos.2307@gmail.com>
2025-03-09 09:49:23 -0300

    temporal CORS

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,10 @@
+// @ts-ignore
 import express, { Request, Response } from "express";
+import cors from "cors";
 import data from "../data/data.json";
 
 const app = express();
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json(data);
